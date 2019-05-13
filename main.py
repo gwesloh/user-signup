7b3d06e75849f2fc0dc36fe0e43db4d947a0307c
GREG WESLOH <GWESLOH@sbcglobal.net>
2019-05-13 14:38:23 -0500

    made corrections

--- main.py
+++ main.py
@@ -36,56 +36,79 @@ def signup():
         email = request.form['email']
         password = request.form['password']
         verify = request.form['verify']
-
+        is_error= False
         if not char_present(username):
             flash('oh no! Username field is blank', category= 'username_error')
-            return render_template('index.html')
-        elif not char_present(password):
+            is_error= True
+    
+        if not char_present(password):
             flash('oh no! Password field is blank', category= 'password_error')
-            return render_template('index.html')
-        elif not char_present(verify):
+            is_error= True
+    
+        if not char_present(verify):
             flash('oh no! Verify field is blank', category= 'verify_error')
-            return render_template('index.html')
-        elif not is_email(email):
+            is_error= True
+    
+        if not is_email(email):
             flash('oh no! "' + email + '" does not seem like an email address', category = 'email_error')
-            return render_template('index.html')
-        elif not is_username_or_pass(username):
+            is_error= True
+    
+        if not is_username_or_pass(username):
             flash('oh no! "' + username + '" does not seem like a username', category= 'username_error')
-            return render_template('index.html')
-        elif not is_username_or_pass(password):
+            is_error= True
+        
+        if not is_username_or_pass(password):
             flash('oh no! "' + password + '" does not seem like a password', category= 'password_error')
-            return render_template('index.html')
-        elif password != verify:
+            is_error= True
+        
+        if password != verify:
             flash('passwords did not match', category= 'verify_error')
+            is_error= True
+
+        if is_error:
             return render_template('index.html')
         else:
-            
             user = User(email=email, password=password, username=username )
             session['user'] = user.username
             db.session.add(user)
             db.session.commit()
             User.query.filter_by(username=username)
-        return render_template('welcome.html', title= 'Welcome Page', username= username)
+            return render_template('welcome.html', title= 'Welcome Page', username= username)
 
 
 def is_email(string):
    
     atsign_index = string.find('@')
-    atsign_present = atsign_index >= 0
-    char_not_present = len(string) == 0 
-    email_present = len(string) >= 3
     space_index = string.find(' ')
-    space_present = space_index >=0
-    if not char_not_present and not email_present:
-        return False
-    elif not atsign_present:
-        return False
-    elif space_present:
-        return False
+    domain_dot_index = string.find('.')
+    if len(string) >= 3:
+        email_present= True
     else:
-        domain_dot_index = string.find('.', atsign_index)
-        domain_dot_present = domain_dot_index >= 0
-        return domain_dot_present
+        email_present= False
+    if atsign_index >= 0:
+        atsign_present= True
+    else:
+        atsign_present= False
+    if space_index == 0:
+        space_not_present = True
+    else:
+        space_not_present = False
+    if domain_dot_index >= 0:
+        domain_dot_present = True
+    else:
+        domain_dot_present = False
+    
+    
+
+    if len(string) == 0:
+        char_not_present = True
+        return True
+    
+    if len(string) != 0:
+        if email_present== True and atsign_present== True and space_not_present== True and domain_dot_present== True:
+            return True
+        else: 
+            return False
 
 def is_username_or_pass(string):
    
